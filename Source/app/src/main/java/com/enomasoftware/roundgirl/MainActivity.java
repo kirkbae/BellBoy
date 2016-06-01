633a7132be2ceff69488cf9d927bdb92e44209dd
Kirk <kirkbae@gmail.com>
2016-05-31 19:16:36 -0700

    Add settings menu.

--- Source/app/src/main/java/com/enomasoftware/roundgirl/MainActivity.java
+++ Source/app/src/main/java/com/enomasoftware/roundgirl/MainActivity.java
@@ -1,9 +1,13 @@
 package com.enomasoftware.roundgirl;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -32,6 +36,25 @@ public class MainActivity extends AppCompatActivity {
         mConfiguration = buildConfiguration();
         mEventHandler = buildEventHandler();
         mBellBoy = buildBellBoy(mConfiguration, mEventHandler);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void btnStart_onClick(View view) {
